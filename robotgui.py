8164df1691cc8716098de9490f37baa428a91c98
RenSkywalker <80165919+RenSkywalker@users.noreply.github.com>
2025-04-11 03:05:45 -0300

    Update robotgui.py

--- robotgui.py
+++ robotgui.py
@@ -72,5 +72,5 @@ def ir_login():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8501, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
